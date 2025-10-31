a6e7d4de3340ed176314172084b2238e5bc48d2c
Siham <96974791+aliancode@users.noreply.github.com>
2025-10-31 23:09:47 +0000

    Create build_medstream.sh

---
+++ scripts/build_medstream.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "1. Download ChestX-ray14 from https://nihcc.app.box.com/v/ChestXray-NIHCC"
+echo "2. Download ISIC 2019 from https://challenge.isic-archive.com/data/"
+echo "3. Place both datasets in data/medstream7k/"
+echo "4. Run preprocessing script (to be implemented by user)"
