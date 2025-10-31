749d8a9e542a7c1688fa56929e3632e709b78b96
Siham <96974791+aliancode@users.noreply.github.com>
2025-10-31 22:39:52 +0000

    Update base.py

--- data/base.py
+++ data/base.py
@@ -1,1 +1,17 @@
+from abc import ABC, abstractmethod
+from torch.utils.data import Dataset
 
+class ContinualDataset(ABC):
+    @abstractmethod
+    def get_task(self, task_id: int) -> Dataset:
+        pass
+
+    @property
+    @abstractmethod
+    def num_tasks(self) -> int:
+        pass
+
+    @property
+    @abstractmethod
+    def class_names_per_task(self):
+        pass
